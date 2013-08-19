991c3d211b358b45183a261b1a54eba04ef1a501
John Blackbourn <johnbillion@gmail.com>
2013-08-19 18:06:59 +0100

    Updates tests to support Multisite

--- tests/test-user-switching.php
+++ tests/test-user-switching.php
@@ -13,18 +13,46 @@ class User_Switching_Test extends WP_UnitTestCase {
 			'role' => 'editor'
 		) );
 
+		if ( is_multisite() ) {
+			$this->super = $this->factory->user->create_and_get( array(
+				'role' => 'administrator'
+			) );
+			grant_super_admin( $this->super->ID );
+		}
+
 	}
 
 	function testCaps() {
 
-		# Can admins switch to editors?
-		$this->assertTrue( user_can( $this->admin->ID, 'switch_to_user', $this->editor->ID ) );
+		if ( is_multisite() ) {
 
-		# Can editors switch to admins?
-		$this->assertFalse( user_can( $this->editor->ID, 'switch_to_user', $this->admin->ID ) );
+			# Can super admins switch to admins?
+			$this->assertTrue( user_can( $this->super->ID, 'switch_to_user', $this->admin->ID ) );
 
-		# Can a user switch to themselves?
-		$this->assertFalse( user_can( $this->admin->ID, 'switch_to_user', $this->admin->ID ) );
+			# Can admins switch to editors?
+			$this->assertFalse( user_can( $this->admin->ID, 'switch_to_user', $this->editor->ID ) );
+
+			# Can editors switch to admins?
+			$this->assertFalse( user_can( $this->editor->ID, 'switch_to_user', $this->admin->ID ) );
+
+			# Can admins switch to super admins?
+			$this->assertFalse( user_can( $this->admin->ID, 'switch_to_user', $this->super->ID ) );
+
+			# Can a super admin switch to themselves?
+			$this->assertFalse( user_can( $this->super->ID, 'switch_to_user', $this->super->ID ) );
+
+		} else {
+
+			# Can admins switch to editors?
+			$this->assertTrue( user_can( $this->admin->ID, 'switch_to_user', $this->editor->ID ) );
+
+			# Can editors switch to admins?
+			$this->assertFalse( user_can( $this->editor->ID, 'switch_to_user', $this->admin->ID ) );
+
+			# Can an admin switch to themselves?
+			$this->assertFalse( user_can( $this->admin->ID, 'switch_to_user', $this->admin->ID ) );
+
+		}
 
 	}
 
