66a8c9480b44a0bd5d359b2ff0d2058bd71653cc
John Blackbourn <johnbillion@gmail.com>
2015-08-09 23:27:36 +0100

    Add back the 'User Switching' heading on the user profile screen which was accidentally removed in 7cd5dd8.

--- user-switching.php
+++ user-switching.php
@@ -91,7 +91,7 @@ class user_switching {
 
 		?>
 		<tr>
-			<th scope="row"><?php esc_html_x( 'User Switching', 'User Switching title on user profile screen', 'user-switching' ); ?></th>
+			<th scope="row"><?php echo esc_html_x( 'User Switching', 'User Switching title on user profile screen', 'user-switching' ); ?></th>
 			<td><a href="<?php echo esc_url( $link ); ?>"><?php esc_html_e( 'Switch&nbsp;To', 'user-switching' ); ?></a></td>
 		</tr>
 		<?php
