7dbaacfbacea887f38712e306fad743ed3bdc480
John Blackbourn <johnbillion@gmail.com>
2015-10-05 20:35:05 +0100

    Introduce `compat.php` for our unit tests.

--- tests/includes/bootstrap.php
+++ tests/includes/bootstrap.php
@@ -6,9 +6,11 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../user-switching.php';
+	require dirname( dirname( dirname( __FILE__ ) ) ) . '/user-switching.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
-require dirname( __FILE__ ) . '/user-switching-test.php';
+
+require dirname( __FILE__ ) . '/compat.php';
+require dirname( dirname( __FILE__ ) ) . '/user-switching-test.php';
