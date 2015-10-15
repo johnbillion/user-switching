432a96fb5f5b9e0554550993a7a786df83aa0f03
John Blackbourn <johnbillion@gmail.com>
2015-10-15 20:11:54 +0100

    Remove compat.php from our tests and update to WP-CLI's latest version of the test install script.

--- tests/includes/bootstrap.php
+++ tests/includes/bootstrap.php
@@ -12,5 +12,4 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
-require dirname( __FILE__ ) . '/compat.php';
 require dirname( dirname( __FILE__ ) ) . '/user-switching-test.php';
