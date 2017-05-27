3cbd95f7a5f679d4adca22603f62ba1306c6efc8
John Blackbourn <johnbillion@gmail.com>
2017-05-27 12:48:37 +0100

    Move linting into the test script.

--- bin/script.sh
+++ bin/script.sh
@@ -4,6 +4,12 @@ function version {
 	echo "$@" | gawk -F. '{ printf("%03d%03d%03d\n", $1,$2,$3); }';
 }
 
+# Lint all the PHP files for syntax errors:
+# (This is primarily used to ensure newer PHP syntax isn't accidentally used.)
+if find . -not \( -path ./vendor -prune \) -not \( -path ./features -prune \) -name "*.php" -exec php -l {} \; | grep "^[Parse error|Fatal error]"; then
+	exit 1;
+fi;
+
 phpv=(`php -v`)
 ver=${phpv[1]}
 
