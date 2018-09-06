80c40aac77e58edc72e6c91662ee6e1aaebdb2b1
John Blackbourn <johnbillion@gmail.com>
2018-09-06 14:31:56 +0100

    Extend the testing to cover sessions for the user being switched to.

--- tests/test-sessions.php
+++ tests/test-sessions.php
@@ -2,34 +2,42 @@
 
 class User_Switching_Test_Sessions extends User_Switching_Test {
 
-	public function testExtraSessionsAreNotCreatedForOldUserWhenSwitching() {
+	public function testExtraSessionsAreNotCreatedForUsersWhenSwitching() {
 		if ( is_multisite() ) {
 			$admin = self::$testers['super'];
 		} else {
 			$admin = self::$testers['admin'];
 		}
 
-		// Set up the session
-		$manager = WP_Session_Tokens::get_instance( $admin->ID );
-		$token   = $manager->create( time() + DAY_IN_SECONDS );
-		$before  = $manager->get_all();
+		// Set up the admin session manager with a session
+		$admin_manager = WP_Session_Tokens::get_instance( $admin->ID );
+		$admin_token   = $admin_manager->create( time() + DAY_IN_SECONDS );
+		$admin_before  = $admin_manager->get_all();
 
-		// Set up the user state
+		// Set up the author session manager, but with no session
+		$author_manager = WP_Session_Tokens::get_instance( self::$users['author']->ID );
+		$author_before  = $author_manager->get_all();
+
+		// Set up the admin user state
 		wp_set_current_user( $admin->ID );
-		wp_set_auth_cookie( $admin->ID, false, '', $token );
+		wp_set_auth_cookie( $admin->ID, false, '', $admin_token );
 
 		// Sanity checks
-		$this->assertNotEmpty( $token );
+		$this->assertNotEmpty( $admin_token );
 		$this->assertNotEmpty( wp_get_session_token() );
 
-		// Start with a base session
-		$this->assertCount( 1, $before );
+		// Verify the initial state
+		$this->assertCount( 1, $admin_before );
+		$this->assertCount( 0, $author_before );
 
 		// Switch user
 		$user = $this->switch_to_user( self::$users['author']->ID );
 
 		// Verify no new sessions were created for the old user
-		$this->assertCount( 1, $manager->get_all() );
+		$this->assertCount( 1, $admin_manager->get_all() );
+
+		// Verify only one new session was created for the new user
+		$this->assertCount( 1, $author_manager->get_all() );
 	}
 
 }
