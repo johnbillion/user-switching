9eea4bca7c349903f1a1af2fb3fd39d9e319f304
John Blackbourn <johnbillion@gmail.com>
2018-09-13 16:53:37 +0100

    Add a test to verify that an invalid session does not get reused nor cause a new session to be created.

--- tests/test-sessions.php
+++ tests/test-sessions.php
@@ -114,4 +114,45 @@ class User_Switching_Test_Sessions extends User_Switching_Test {
 		$this->assertCount( 1, $admin_manager->get_all() );
 	}
 
+	public function testExpiredSessionPreventsUserFromSwitchingBack() {
+		if ( is_multisite() ) {
+			$admin = self::$testers['super'];
+		} else {
+			$admin = self::$testers['admin'];
+		}
+
+		// Set up the admin session manager with a session
+		$admin_manager = WP_Session_Tokens::get_instance( $admin->ID );
+		$admin_token   = $admin_manager->create( time() + DAY_IN_SECONDS );
+		$admin_before  = $admin_manager->get_all();
+
+		// Set up the admin user state
+		wp_set_current_user( $admin->ID );
+		wp_set_auth_cookie( $admin->ID, false, '', $admin_token );
+
+		// Verify the initial state
+		$this->assertCount( 1, $admin_before );
+
+		// Switch user
+		$user = $this->switch_to_user( self::$users['author']->ID );
+
+		// Verify no new sessions were created for the old user
+		$this->assertCount( 1, $admin_manager->get_all() );
+
+		// Invalidate the session that the user switched from, to mock its expiry while switched
+		$existing = $admin_manager->get( $admin_token );
+		$existing['expiration'] = time() - HOUR_IN_SECONDS;
+		$admin_manager->update( $admin_token, $existing );
+
+		// Sanity checks
+		$this->assertNull( $admin_manager->get( $admin_token ) );
+		$this->assertFalse( user_switching::get_old_user() );
+
+		// Attempt to switch back
+		$user = $this->switch_to_user( $admin->ID, false, false );
+
+		// Verify no new session was created for the original user
+		$this->assertCount( 0, $admin_manager->get_all() );
+	}
+
 }
