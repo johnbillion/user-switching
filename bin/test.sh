e16279997c0320db525aa30cdc29784d429a83df
John Blackbourn <johnbillion@gmail.com>
2019-06-02 16:13:04 +0100

    Formatting.

--- bin/test.sh
+++ bin/test.sh
@@ -9,8 +9,10 @@ WP_CORE_DIR="${PWD}/tests/wordpress"
 php -S localhost:8000 -t "$WP_CORE_DIR" -d disable_functions=mail 2>/dev/null &
 
 ./vendor/bin/wp db reset --yes --color --path="$WP_CORE_DIR"
+
 ./vendor/bin/wp core install --color --path="$WP_CORE_DIR" --url='http://localhost:8000' \
 	--title="Example" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com"
+
 BEHAT_PARAMS='{"extensions" : {"PaulGibbs\\WordpressBehatExtension" : {"path" : "'$WP_CORE_DIR'"}}}' \
 	./vendor/bin/behat --colors
 
