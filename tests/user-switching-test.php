568cefc2b776b6e282b668cbbde590b3b3625343
John Blackbourn <johnbillion@gmail.com>
2021-03-07 12:21:47 +0100

    Correct this test setup.

--- tests/user-switching-test.php
+++ tests/user-switching-test.php
@@ -49,6 +49,8 @@ abstract class User_Switching_Test extends WP_UnitTestCase {
 	}
 
 	public function setUp() {
+		parent::setUp();
+
 		add_action( 'set_auth_cookie',           array( $this, 'action_set_auth_cookie' ), 10, 6 );
 		add_action( 'set_logged_in_cookie',      array( $this, 'action_set_logged_in_cookie' ), 10, 6 );
 		add_action( 'clear_auth_cookie',         array( $this, 'action_clear_auth_cookie' ) );
@@ -56,8 +58,6 @@ abstract class User_Switching_Test extends WP_UnitTestCase {
 		add_action( 'set_user_switching_cookie', array( $this, 'action_set_user_switching_cookie' ), 10, 5 );
 		add_action( 'set_olduser_cookie',        array( $this, 'action_set_olduser_cookie' ), 10, 5 );
 		add_action( 'clear_olduser_cookie',      array( $this, 'action_clear_olduser_cookie' ) );
-
-		parent::setUp();
 	}
 
 	public function action_set_auth_cookie( $cookie, $expire, $expiration, $user_id, $scheme, $token ) {
