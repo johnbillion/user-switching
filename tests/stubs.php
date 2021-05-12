4e144766914cc97175629f2b9051c2ae8884c8da
John Blackbourn <johnbillion@gmail.com>
2021-05-12 16:59:49 +0100

    More bootstrap config.

--- tests/stubs.php
+++ tests/stubs.php
@@ -1,3 +1,12 @@
 <?php
 
+// WP core constants:
+define( 'COOKIE_DOMAIN', '' );
 define( 'COOKIEHASH', '' );
+define( 'COOKIEPATH', '' );
+define( 'SITECOOKIEPATH', '' );
+
+// User Switching constants:
+define( 'USER_SWITCHING_COOKIE', '' );
+define( 'USER_SWITCHING_OLDUSER_COOKIE', '' );
+define( 'USER_SWITCHING_SECURE_COOKIE', '' );
