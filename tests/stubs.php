2386e910b30d5fb4ca2338790ea0fbcb8ca1d45c
John Blackbourn <johnbillion@gmail.com>
2021-05-12 17:08:18 +0100

    Start adding BuddyPress stubs.

--- tests/stubs.php
+++ tests/stubs.php
@@ -10,3 +10,33 @@ define( 'SITECOOKIEPATH', '' );
 define( 'USER_SWITCHING_COOKIE', '' );
 define( 'USER_SWITCHING_OLDUSER_COOKIE', '' );
 define( 'USER_SWITCHING_SECURE_COOKIE', '' );
+
+/**
+ * @return bool
+ */
+function bp_is_user() {
+}
+
+/**
+ * @return int
+ */
+function bp_displayed_user_id() {
+}
+
+/**
+ * @return bool
+ */
+function bp_is_members_directory() {
+}
+
+/**
+ * @return int
+ */
+function bp_get_member_user_id() {
+}
+
+/**
+ * @return string
+ */
+function bp_core_get_user_domain( int $user_id ) {
+}
