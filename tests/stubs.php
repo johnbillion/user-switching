a2c4f3c617a6ca678bc2ff63f1a62b638d6ac862
John Blackbourn <johnbillion@gmail.com>
2021-05-12 20:42:58 +0100

    Bootstrap some more WP constants.

--- tests/stubs.php
+++ tests/stubs.php
@@ -1,9 +1,12 @@
 <?php
 
 // WP core constants:
+define( 'AUTH_COOKIE', '' );
 define( 'COOKIE_DOMAIN', '' );
 define( 'COOKIEHASH', '' );
 define( 'COOKIEPATH', '' );
+define( 'LOGGED_IN_COOKIE', '' );
+define( 'SECURE_AUTH_COOKIE', '' );
 define( 'SITECOOKIEPATH', '' );
 
 // User Switching constants:
