98fcdaa19d31d5884b9c5ffcc71cfd97af39e0ab
John Blackbourn <john@humanmade.com>
2022-06-24 18:18:49 +0100

    Version 1.6.0.

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # User Switching
 
-Stable tag: 1.5.8  
+Stable tag: 1.6.0  
 Requires at least: 3.7  
 Tested up to: 6.0  
 Requires PHP: 5.3  
--- user-switching.php
+++ user-switching.php
@@ -10,7 +10,7 @@
  *
  * Plugin Name:  User Switching
  * Description:  Instant switching between user accounts in WordPress
- * Version:      1.5.8
+ * Version:      1.6.0
  * Plugin URI:   https://wordpress.org/plugins/user-switching/
  * Author:       John Blackbourn & contributors
  * Author URI:   https://github.com/johnbillion/user-switching/graphs/contributors
