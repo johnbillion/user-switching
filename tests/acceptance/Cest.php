11c3967256447a7b9de17b0188a477ed272be186
John Blackbourn <john@humanmade.com>
2022-07-14 01:40:14 +0100

    Set a determinate permalink structure during installation.

--- tests/acceptance/Cest.php
+++ tests/acceptance/Cest.php
@@ -9,6 +9,7 @@ class Cest {
 
 		# Install WordPress:
 		$I->cli( 'core install --title="Example" --admin_user="admin" --admin_password="admin" --admin_email="admin@example.com" --skip-email' );
+		$I->cli( 'rewrite structure "%postname%"' );
 
 		# Activate the plugin:
 		$I->cli( 'plugin activate user-switching' );
