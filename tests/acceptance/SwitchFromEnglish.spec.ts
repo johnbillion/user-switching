63ce5dbcc46073fb50ebc6e2c329de1409e789f6
John Blackbourn <john@johnblackbourn.com>
2025-08-28 01:45:30 +0100

    Indentation.

--- tests/acceptance/SwitchFromEnglish.spec.ts
+++ tests/acceptance/SwitchFromEnglish.spec.ts
@@ -3,54 +3,52 @@ import { GlobalUtils } from './utils/global-utils';
 import * as semver from 'semver';
 
 test.describe( 'Switch From English', () => {
-	test.describe( 'WordPress 6.2+', () => {
-		test.beforeAll( async ( { globalUtils } ) => {
-			// Install WordPress fresh for this test suite
-			globalUtils.installWordPress();
+	test.beforeAll( async ( { globalUtils } ) => {
+		// Install WordPress fresh for this test suite
+		globalUtils.installWordPress();
+	} );
+
+	test( 'Switch from English admin to Italian author and back', {
+		annotation: {
+			type: 'user-story',
+			description: 'As an administrator of a site which uses more than one language, I need to be able to switch to user accounts that use a different language, and see the output of User Switching in my original language'
+		}
+	}, async ( {
+		page,
+		admin,
+		userSwitching,
+	}, testInfo ) => {
+		// Check WordPress version - this test requires 6.2+
+		const wpVersion = GlobalUtils.runWPCLICommand( 'core version' );
+		console.log( `Running test on WordPress version ${wpVersion}` );
+
+		testInfo.skip( semver.lt( wpVersion, '6.2.0' ), 'This test requires WordPress 6.2 or later' );
+
+		// Create Italian author user
+		userSwitching.createUser( 'autore', 'author', {
+			name: 'Autore',
+			locale: 'it_IT',
 		} );
 
-		test( 'Switch from English admin to Italian author and back', {
-			annotation: {
-				type: 'user-story',
-				description: 'As an administrator of a site which uses more than one language, I need to be able to switch to user accounts that use a different language, and see the output of User Switching in my original language'
-			}
-		}, async ( {
-			page,
-			admin,
-			userSwitching,
-		}, testInfo ) => {
-			// Check WordPress version - this test requires 6.2+
-			const wpVersion = GlobalUtils.runWPCLICommand( 'core version' );
-			console.log( `Running test on WordPress version ${wpVersion}` );
+		// Login as admin (English)
+		await userSwitching.loginViaPage( 'admin', 'password' );
+		await admin.visitAdminPage( '/' );
 
-			testInfo.skip( semver.lt( wpVersion, '6.2.0' ), 'This test requires WordPress 6.2 or later' );
+		// Switch to Italian author
+		await userSwitching.switchToUser( 'autore' );
+		await userSwitching.canSeeThePageInLanguage( 'it-IT' );
+		await userSwitching.seeAdminSuccessNotice( 'Switched to Autore.' );
 
-			// Create Italian author user
-			userSwitching.createUser( 'autore', 'author', {
-				name: 'Autore',
-				locale: 'it_IT',
-			} );
+		// The user switching element should be in English
+		const switchingElement = page.locator( '#user_switching p' );
+		await expect( switchingElement ).toHaveAttribute( 'lang', 'en-US' );
 
-			// Login as admin (English)
-			await userSwitching.loginViaPage( 'admin', 'password' );
-			await admin.visitAdminPage( '/' );
+		// Go to dashboard
+		await admin.visitAdminPage( '/' );
 
-			// Switch to Italian author
-			await userSwitching.switchToUser( 'autore' );
-			await userSwitching.canSeeThePageInLanguage( 'it-IT' );
-			await userSwitching.seeAdminSuccessNotice( 'Switched to Autore.' );
-
-			// The user switching element should be in English
-			const switchingElement = page.locator( '#user_switching p' );
-			await expect( switchingElement ).toHaveAttribute( 'lang', 'en-US' );
-
-			// Go to dashboard
-			await admin.visitAdminPage( '/' );
-
-			// Switch back to English admin
-			await userSwitching.switchBackTo( 'admin' );
-			await userSwitching.canSeeThePageInLanguage( 'en-US' );
-			await userSwitching.seeAdminSuccessNotice( 'Switched back to admin.' );
-		} );
+		// Switch back to English admin
+		await userSwitching.switchBackTo( 'admin' );
+		await userSwitching.canSeeThePageInLanguage( 'en-US' );
+		await userSwitching.seeAdminSuccessNotice( 'Switched back to admin.' );
 	} );
 } );
--- tests/acceptance/SwitchToEnglish.spec.ts
+++ tests/acceptance/SwitchToEnglish.spec.ts
@@ -3,58 +3,56 @@ import { GlobalUtils } from './utils/global-utils';
 import * as semver from 'semver';
 
 test.describe( 'Switch To English', () => {
-	test.describe( 'WordPress 6.2+', () => {
-		test.beforeAll( async ( { globalUtils } ) => {
-			// Install WordPress fresh for this test suite
-			globalUtils.installWordPress();
+	test.beforeAll( async ( { globalUtils } ) => {
+		// Install WordPress fresh for this test suite
+		globalUtils.installWordPress();
+	} );
+
+	test( 'Switch from Italian admin to English author and back', {
+		annotation: {
+			type: 'user-story',
+			description: 'As an administrator of a site which uses more than one language, I need to be able to switch between users, and see the output of User Switching in my original language'
+		}
+	}, async ( {
+		page,
+		admin,
+		userSwitching,
+	}, testInfo ) => {
+		// Check WordPress version - this test requires 6.2+
+		const wpVersion = GlobalUtils.runWPCLICommand( 'core version' );
+		console.log( `Running test on WordPress version ${wpVersion}` );
+
+		testInfo.skip( semver.lt( wpVersion, '6.2.0' ), 'This test requires WordPress 6.2 or later' );
+
+		// Create Italian admin user
+		await userSwitching.createUser( 'admin_it', 'administrator', {
+			name: 'Admin IT',
+			locale: 'it_IT',
 		} );
 
-		test( 'Switch from Italian admin to English author and back', {
-			annotation: {
-				type: 'user-story',
-				description: 'As an administrator of a site which uses more than one language, I need to be able to switch between users, and see the output of User Switching in my original language'
-			}
-		}, async ( {
-			page,
-			admin,
-			userSwitching,
-		}, testInfo ) => {
-			// Check WordPress version - this test requires 6.2+
-			const wpVersion = GlobalUtils.runWPCLICommand( 'core version' );
-			console.log( `Running test on WordPress version ${wpVersion}` );
-
-			testInfo.skip( semver.lt( wpVersion, '6.2.0' ), 'This test requires WordPress 6.2 or later' );
-
-			// Create Italian admin user
-			await userSwitching.createUser( 'admin_it', 'administrator', {
-				name: 'Admin IT',
-				locale: 'it_IT',
-			} );
-
-			// Create English author user
-			await userSwitching.createUser( 'author_en', 'author', {
-				name: 'Author EN',
-			} );
-
-			// Login as Italian admin
-			await userSwitching.loginViaPage( 'admin_it', 'password' );
-
-			// Switch to English author
-			await userSwitching.switchToUser( 'author_en' );
-			await userSwitching.canSeeThePageInLanguage( 'en-US' );
-			await userSwitching.seeAdminSuccessNotice( 'Cambiato a Author EN.' );
-
-			// The user switching element should be in Italian
-			const switchingElement = page.locator( '#user_switching p' );
-			await expect( switchingElement ).toHaveAttribute( 'lang', 'it-IT' );
-
-			// Go to dashboard
-			await admin.visitAdminPage( '/' );
-
-			// Switch back to Italian admin
-			await userSwitching.switchBackTo( 'admin_it', 'it-IT' );
-			await userSwitching.canSeeThePageInLanguage( 'it-IT' );
-			await userSwitching.seeAdminSuccessNotice( 'Tornato a Admin IT.' );
+		// Create English author user
+		await userSwitching.createUser( 'author_en', 'author', {
+			name: 'Author EN',
 		} );
+
+		// Login as Italian admin
+		await userSwitching.loginViaPage( 'admin_it', 'password' );
+
+		// Switch to English author
+		await userSwitching.switchToUser( 'author_en' );
+		await userSwitching.canSeeThePageInLanguage( 'en-US' );
+		await userSwitching.seeAdminSuccessNotice( 'Cambiato a Author EN.' );
+
+		// The user switching element should be in Italian
+		const switchingElement = page.locator( '#user_switching p' );
+		await expect( switchingElement ).toHaveAttribute( 'lang', 'it-IT' );
+
+		// Go to dashboard
+		await admin.visitAdminPage( '/' );
+
+		// Switch back to Italian admin
+		await userSwitching.switchBackTo( 'admin_it', 'it-IT' );
+		await userSwitching.canSeeThePageInLanguage( 'it-IT' );
+		await userSwitching.seeAdminSuccessNotice( 'Tornato a Admin IT.' );
 	} );
 } );
