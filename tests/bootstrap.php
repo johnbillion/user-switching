48ea2f69cde063aad9ba53074cebd7a9f67f7430
John Blackbourn <john@johnblackbourn.com>
2025-08-30 00:30:28 +0100

    More simplification.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,13 +3,7 @@
 // Load the Composer autoloader first
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
-// Use WP_TESTS_DIR environment variable if set, otherwise use wp-phpunit
-if ( getenv( 'WP_TESTS_DIR' ) ) {
-	$_tests_dir = rtrim( getenv( 'WP_TESTS_DIR' ), '/' ) . '/';
-} else {
-	// Default path using wp-phpunit package
-	$_tests_dir = dirname( __DIR__ ) . '/vendor/wp-phpunit/wp-phpunit/';
-}
+$_tests_dir = dirname( __DIR__ ) . '/vendor/wp-phpunit/wp-phpunit/';
 
 // Get access to tests_add_filter() function
 require_once $_tests_dir . 'includes/functions.php';
