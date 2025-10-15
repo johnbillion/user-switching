4fa50ba0a9ce2f7dacacf46154cc4f87e8f44c59
John Blackbourn <john@johnblackbourn.com>
2025-10-15 22:01:47 +0100

    Add a test for the switch back link on the `wp_die()` screen.

--- tests/acceptance/AccessDenied.spec.ts
+++ tests/acceptance/AccessDenied.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from './utils/test-setup';
 
 test.describe( 'Access Denied', () => {
-	test.beforeAll( async ( { globalUtils } ) => {
+	test.beforeEach( async ( { globalUtils } ) => {
 		await globalUtils.installWordPress();
 	} );
 
-	test( 'Switch back from page access denied', {
+	test( 'Switch back from admin screen access denied', {
 		annotation: {
 			type: 'user-story',
-			description: 'As a user who has switched accounts, I want to see a Switch Back link on "access denied" screens, in order to quickly switch back to my original account'
+			description: 'As a user who has switched accounts, I want to see a Switch Back link when I am denied access to an admin screen, in order to quickly switch back to my original account'
 		}
 	}, async ( {
 		page,
@@ -25,7 +25,7 @@ test.describe( 'Access Denied', () => {
 		// Switch to editor
 		await userSwitching.switchToUser( 'editor' );
 
-		// Try to access a non-existent admin page (which editor can't access)
+		// Try to access an admin screen which the editor can't access
 		await admin.visitAdminPage( 'tools.php', 'page=foo' );
 
 		// Should see error in title
@@ -34,8 +34,44 @@ test.describe( 'Access Denied', () => {
 		// Switch back to admin
 		await userSwitching.switchBackTo( 'admin' );
 
-		// Should be on the same page but with switched parameters
+		// Should be on the same admin screen but with switched parameters
 		const expectedPath = '/wp-admin/tools.php?page=foo&user_switched=true&switched_back=true';
 		expect( page.url() ).toContain( expectedPath );
 	} );
-} );
+
+	test( 'Switch back from item access denied', {
+		annotation: {
+			type: 'user-story',
+			description: 'As a user who has switched accounts, I want to see a Switch Back link when I am denied access to an item, in order to quickly switch back to my original account'
+		}
+	}, async ( {
+		page,
+		admin,
+		userSwitching,
+		globalUtils,
+	}, testInfo ) => {
+		// Check WordPress version - this test requires 6.1+
+		testInfo.skip( ! globalUtils.isWordPressVersionAtLeast( 6.1 ), 'This test requires WordPress 6.1 or later' );
+
+		// Create editor user for testing
+		globalUtils.runWPCLICommand( 'user create editor editor@example.com --role=editor --user_pass=password' );
+
+		// Login through the page's browser context
+		await userSwitching.loginViaPage( 'admin', 'password' );
+
+		// Switch to editor
+		await userSwitching.switchToUser( 'editor' );
+
+		// Try to edit an item which the editor can't access
+		await admin.visitAdminPage( 'post.php', 'post=12345&action=edit' );
+
+		// Should see error in title
+		await expect( page ).toHaveTitle( /Error/ );
+
+		// Switch back to admin
+		await userSwitching.switchBackTo( 'admin' );
+
+		// Should be on the same page but with switched parameters
+		const expectedPath = '/wp-admin/post.php?post=12345&action=edit&user_switched=true&switched_back=true';
+		expect( page.url() ).toContain( expectedPath );
+	} );} );
