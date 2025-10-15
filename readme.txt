88d9959022b543fe1b84eda582429647e6121361
John Blackbourn <john@johnblackbourn.com>
2025-10-15 23:31:04 +0100

    Version 1.11.0.

--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 # User Switching
 
-Stable tag: 1.10.0
+Stable tag: 1.11.0
 Tested up to: 6.8
 License: GPL v2 or later
 Tags: users, user switching, fast user switching, multisite, woocommerce
--- user-switching.php
+++ user-switching.php
@@ -10,7 +10,7 @@
  *
  * Plugin Name:       User Switching
  * Description:       Instant switching between user accounts in WordPress
- * Version:           1.10.0
+ * Version:           1.11.0
  * Plugin URI:        https://wordpress.org/plugins/user-switching/
  * Author:            John Blackbourn
  * Author URI:        https://johnblackbourn.com
