45a3f8e9e4c43aa2d48718bf07b3bc29d47a9ed5
John Blackbourn <john@johnblackbourn.com>
2025-12-04 16:45:23 +0100

    Add the plugin ID.

--- user-switching.php
+++ user-switching.php
@@ -12,6 +12,7 @@
  * Description:       Instant switching between user accounts in WordPress
  * Version:           1.11.0
  * Plugin URI:        https://wordpress.org/plugins/user-switching/
+ * Plugin ID:         did:plc:ap4bi3w5w42oy7hfom57tehj
  * Author:            John Blackbourn
  * Author URI:        https://johnblackbourn.com
  * Text Domain:       user-switching
